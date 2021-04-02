18e0a626100caa247ccb01451c1900a482b9d7eb
Nurzhan Nogerbek <nurzhannogerbek@gmail.com>
2021-04-02 16:48:00 +0600

    BT-635

--- internal/repository/postgrsql/channel.go
+++ internal/repository/postgrsql/channel.go
@@ -74,10 +74,12 @@ func (cr *ChannelRepository) GetChannels(organizationId *string) (*[]common.Chan
 			channels
 		left join channels_organizations_relationship on
 			channels.channel_id = channels_organizations_relationship.channel_id
+		left join organizations on
+			channels_organizations_relationship.organization_id = organizations.organization_id
 		where
-			channels_organizations_relationship.organization_id = $1
+			organizations.tree_organization_id like concat( '%', '\', $1, '%' )
 		group by
-    		channels.channel_id;`, organizationId)
+			channels.channel_id;`, organizationId)
 	if err != nil {
 		return nil, err
 	}
