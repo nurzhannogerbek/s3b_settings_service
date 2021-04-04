67bb7ac64bccc330676cba940b27d4dca09004c6
asetriza <57208705+asetriza@users.noreply.github.com>
2021-04-05 00:14:18 +0600

    BT-637

--- cmd/awslambda/organization/getallorganizationdepartmentspublic/main.go
+++ cmd/awslambda/organization/getallorganizationdepartmentspublic/main.go
@@ -5,7 +5,6 @@ import (
 	"bitbucket.org/3beep-workspace/3beep_settings_service/internal/repository"
 	"bitbucket.org/3beep-workspace/3beep_settings_service/internal/service"
 	"bitbucket.org/3beep-workspace/3beep_settings_service/pkg/database/postgresql"
-	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 
 	"github.com/aws/aws-lambda-go/lambda"
@@ -35,31 +34,31 @@ type OrganizationEvent struct {
 }
 
 func handleRequest(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var organizationEvent OrganizationEvent
-	if err := json.Unmarshal([]byte(request.Body), &organizationEvent); err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 404,
-			Body: err.Error(),
-		}, err
-	}
+	//var organizationEvent OrganizationEvent
+	//if err := json.Unmarshal([]byte(request.Body), &organizationEvent); err != nil {
+	//	return events.APIGatewayProxyResponse{
+	//		StatusCode: 404,
+	//		Body: err.Error(),
+	//	}, err
+	//}
+	//
+	//organizations, err := Services.Organization.GetAllOrganizationDepartments(organizationEvent.RootOrganizationID)
+	//if err != nil {
+	//	return events.APIGatewayProxyResponse{
+	//		StatusCode: 404,
+	//		Body: err.Error(),
+	//	}, err
+	//}
+	//
+	//org, err := json.Marshal(&organizations)
+	//if err != nil {
+	//	return events.APIGatewayProxyResponse{
+	//		StatusCode: 404,
+	//		Body: err.Error(),
+	//	}, err
+	//}
 
-	organizations, err := Services.Organization.GetAllOrganizationDepartments(organizationEvent.RootOrganizationID)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 404,
-			Body: err.Error(),
-		}, err
-	}
-
-	org, err := json.Marshal(&organizations)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: 404,
-			Body: err.Error(),
-		}, err
-	}
-
-	return events.APIGatewayProxyResponse{Body: string(org), StatusCode: 200}, nil
+	return events.APIGatewayProxyResponse{Body: "asdsdaasd", StatusCode: 200}, nil
 }
 
 func main() {
